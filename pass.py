273eb22ccccfa803e2ac8d6af7a9651da397abfd
Sofia Vasilatou <118475148+sofiavasilatou@users.noreply.github.com>
2026-01-07 11:23:59 +0200

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,4 +1,4 @@
-from app3 import Database  # your main file name here
+from app import Database  # your main file name here
 
 db = Database()
 
@@ -8,5 +8,6 @@ db.setpasswords(2, "anastasia123")   # waiter_id = 2
 db.setpasswords(3, "takis123")   # waiter_id = 3
 db.setpasswords(4, "eleni123")   # waiter_id = 4
 db.setpasswords(5, "petros123")   # waiter_id = 5
+
 
 print("Passwords updated.")
